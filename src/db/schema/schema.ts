96831635845a1c6ff6a1c875d9a3c332179c7c3f
MrSesbianLexovich <fishersasha122@gmail.com>
2025-11-21 17:58:20 +0300

    .

---
+++ src/db/schema/schema.ts
@@ -0,0 +1,98 @@
+import { relations, sql } from 'drizzle-orm'
+import * as pg from 'drizzle-orm/pg-core'
+
+export const users = pg.pgTable('users', {
+	id: pg
+		.varchar('id', { length: 255 })
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => Bun.randomUUIDv7()),
+	name: pg
+		.text('name')
+		.notNull(),
+	email: pg
+		.text('email')
+		.notNull()
+		.unique(),
+	dateOfBirth: pg
+		.timestamp('dateOfBirth')
+		.notNull()
+})
+
+export const userToCartsAndFavoursRelation = relations(users, ({ many }) => ({
+	carts: many(cart),
+	favours: many(favour)
+}))
+
+export const products = pg.pgTable('products', {
+	id: pg
+		.varchar({length:255})
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => Bun.randomUUIDv7()),
+	imageUrl: pg
+		.text('url')
+		.default('./'),
+	name: pg
+		.text('name')
+		.notNull(),
+	description: pg
+		.text('description')
+		.notNull()
+})
+
+export const favour = pg.pgTable('favourites', {
+	id: pg
+		.varchar('id', {length: 255})
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => Bun.randomUUIDv7()),
+	userId: pg
+		.varchar('userId', {length: 255})
+		.notNull()
+		.references(() => users.id, {onDelete: 'cascade', onUpdate: 'cascade'}),
+	productId: pg
+		.varchar('productId', {length: 255})
+		.notNull()
+		.references(() => products.id, {onDelete: 'cascade', onUpdate: 'cascade'}),
+	createdAt: pg
+		.timestamp('createdAt').notNull().default(sql`now()`)
+})
+
+export const favourToUserAndProductRelation = relations(favour, ({one}) => ({
+	product: one(products, {
+		fields: [favour.productId],
+		references: [products.id]
+	}),
+	user: one(users, {
+		fields: [favour.userId],
+		references: [users.id]
+	})
+}))
+
+export const cart = pg.pgTable('cart', {
+	id: pg
+		.varchar('id', {length: 255})
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => Bun.randomUUIDv7()),
+	productId: pg
+		.varchar('productId', {length: 255})
+		.notNull()
+		.references(() => products.id, {onDelete: 'cascade', onUpdate: 'cascade'}),
+	userId: pg
+		.varchar('userId', {length: 255})
+		.notNull()
+		.references(() => users.id, {onDelete: 'cascade', onUpdate: 'cascade'})
+})
+
+export const cartToUserAndProductRelation = relations(cart, ({ one }) => ({
+	product: one(products, {
+		fields: [cart.productId],
+		references: [products.id]
+	}),
+	user: one(users, {
+		fields: [cart.userId],
+		references: [users.id]
+	})
+}))
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,85 @@
-import "dotenv/config";
-import { eq } from "drizzle-orm";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { productsTable, usersTable } from "./db/schema";
+import { eq } from 'drizzle-orm'
+import { db } from './db'
+import { cart, favour, products, users } from './db/schema/schema'
 
-const db = drizzle(process.env.DATABASE_URL!);
 
-async function createUser(
-	name: string,
-	email: string,
-	birthdate: string,
-	age: number,
-) {
-	const [newUser] = await db
-		.insert(usersTable)
-		.values({
-			name: name,
-			email: email,
-			birthdate: birthdate,
-			age: age,
-		})
-		.returning();
-
-	console.log(newUser);
+async function createUser(body : {name: string, email: string, dateOfBirth: Date}) {
+	const user = await db.insert(users).values(body).returning()
+	console.log(`User successfully created: ${user}`)
 }
 
-async function deleteUser(id: string) {
-	await db.delete(usersTable).where(eq(usersTable.id, id));
+async function updateUser(userId: string, body: {name?: string, email?: string}) {
+	const user = await db.update(users).set(body).where(eq(users.id, userId)).returning()
+	console.log(`User successfully updated: ${user}`)
 }
 
-async function updateUser(id: string, name: string) {
-	await db.update(usersTable).set({ name: name }).where(eq(usersTable.id, id));
+async function deleteUser(userId:string) {
+	await db.delete(users).where(eq(users.id, userId))
+	console.log(`${userId} User successfully deleted`)
 }
 
 async function getUsers() {
-	const users = await db.select().from(usersTable);
-	console.log(users);
+	const res = await db.query.users.findMany({
+		with: {
+			carts:true,
+			favours:true
+		}
+	})
+	console.log(res)
 }
 
-async function createProduct(
-	imageURL: string,
-	name: string,
-	description: string,
-) {
-	await db.insert(productsTable).values({
-		imageURL: imageURL,
-		name: name,
-		description: description,
-	});
+async function createProduct(body: {imageUrl: string, name: string, description: string}) {
+	const product = await db.insert(products).values(body).returning()
+	console.log(`Product successfully created: ${product}`
+	)
 }
 
-// async function addProductToCart(
-// 	productId: string,
-// 	userId: string,
-// 	amount: number,
-// ) {
-// 	await db.insert(cartTable).values({
-// 		productId: productId,
-// 		userId: userId,
-// 		amount: amount,
-// 	});
+async function updateProduct(productId: string, body: {imageUrl?: string, name?: string, description?: string}) {
+	const product = await db.update(products).set(body).where(eq(products.id, productId)).returning()
+	console.log(`Product successfully updated: ${product}`)
+}
 
-// }
+async function deleteProduct(productId:string) {
+	await db.delete(products).where(eq(products.id, productId))
+	console.log(`${productId} Product successfully deleted`)
+}
 
-// createUser("TestName", "email1@gmail.com", "2008-01-14", 17);
+async function getProducts() {
+	const res = await db.query.products.findMany({
+	})
+	console.log(res)
+}
 
-// deleteUser("019a95c5-32f6-7000-8901-a4ee71bd22a9");
+async function addProductToCart(body: {productId:string, userId:string}) {
+	const cartRet = await db.insert(cart).values(body).returning()
+	console.log(`Cart successfully created: ${cartRet}`)
+}
 
-// updateUser("019a95d5-efb3-7000-b26d-51736ed2f3b1", "NewName");
+async function deleteCart(cartId: string) {
+	await db.delete(cart).where(eq(cart.id, cartId))
+}
 
-// getUsers();
+async function addProductToFavourite(body: {productId:string, userId:string}) {
+	const favourite = await db.insert(favour).values(body).returning()
+	console.log(`Favourite product successfully created ${favourite}`)
+}
 
-//createProduct("imageURL", "TestProduct2", "description of a test product");
+async function deleteFavourite(favouriteId: string) {
+	await db.delete(favour).where(eq(favour.id, favouriteId))
+}
+
+
+
+// createUser({name: "TestName", email: "email@gmail.com", dateOfBirth: new Date(2000, 12, 20)})
+
+// getUsers()
+
+// createProduct({imageUrl: "imageurl", name: "product name", description: "product description"})
+
+// updateProduct("019aa0c3-0ffe-7000-b85a-806612101b93", {name: "New name"})
+
+getProducts()
+
+// addProductToCart({productId: "019aa0be-38a1-7000-91e4-e9447606880c", userId: "019aa0a4-bb17-7000-ab34-d747f39b0ee0"})
+
+// addProductToFavourite({productId: "019aa0c3-0ffe-7000-b85a-806612101b93", userId: "019aa0a4-bb17-7000-ab34-d747f39b0ee0"})
